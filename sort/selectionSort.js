79ad1ea0fad8a1f90ce0f191f6f48e97983dbc25
lijin <tjuking@gmail.com>
2015-07-18 22:44:18 +0800

    选择排序

---
+++ sort/selectionSort.js
@@ -0,0 +1,18 @@
+//选择排序
+function selectionSort(data){
+    var len = data.length;
+    var min;
+    var temp;
+    console.log("Origin:", data);
+    for(var i=0; i<=len-2; i++){
+        min = i;
+        for(var j=i+1; j<=len-1; j++){
+            if(data[j] < data[min]){
+                temp = data[min];
+                data[min] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
+    console.log("Result:", data);
+}
